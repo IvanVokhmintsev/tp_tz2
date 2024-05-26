37d0444aefd8f32ada55ee6a4518a1cd0349e935
Ivan Vokhmintsev <vokhmintsev.vanya@gmail.com>
2024-05-26 23:33:06 +0300

    Check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.stream.Collectors;
 
 public class Main {
+    // Here
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             throw new IOException();
